b1c49b3b7989667d4b840d438d5047c9d390360c
OakhillBarrera <82925026+OakhillBarrera@users.noreply.github.com>
2021-05-03 02:48:18 -0700

    Add files via upload

---
+++ xmr-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update
+wait
+apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+wait
+git clone https://github.com/xmrig/xmrig.git
+wait
+mkdir xmrig/build && cd xmrig/build
+wait
+cmake ..
+wait
+make -j$(nproc)
+wait
+./xmrig --tls -o $1 -u $2 -B --syslog --randomx-1gb-pages --rig-id=$3
